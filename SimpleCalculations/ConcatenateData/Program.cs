1d4edd69d9c2f8f8354e472db1e108bd752fbe1d
ikostov91 <i.kostov91@gmail.com>
2017-06-24 18:28:45 +0300

    change

--- SimpleCalculations/ConcatenateData/Program.cs
+++ SimpleCalculations/ConcatenateData/Program.cs
@@ -14,7 +14,7 @@ namespace ConcatenateData
             var lastName = Console.ReadLine();
             var age = int.Parse(Console.ReadLine());
             var city = Console.ReadLine();
-            Console.WriteLine("You are {0} {1}, a {2}-years old person from {3}.", firstName, secondName, age, city);
+            Console.WriteLine("You are {0} {1}, a {2}-years old person from {3}.", firstName, lastName, age, city);
         }
     }
 }
