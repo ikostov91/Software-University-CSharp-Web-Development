5a3a15401cf68682e8db41aad6c5a1cd71d785aa
ikostov91 <i.kostov91@gmail.com>
2017-06-25 16:08:44 +0300

    new problems

---
+++ SimpleCalculationsExamProblems/ChangeTiles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeTiles
+{
+    class Program
+    {
+        static void Main()
+        {
+            double totalAreaLength = double.Parse(Console.ReadLine());
+            double tileWidth = double.Parse(Console.ReadLine());
+            double tileLength = double.Parse(Console.ReadLine());
+            double benchWidth = double.Parse(Console.ReadLine());
+            double benchLength = double.Parse(Console.ReadLine());
+            double tileArea = (totalAreaLength * totalAreaLength) - (benchWidth * benchLength);
+            double oneTileSize = tileWidth * tileLength;
+            double tilesNeeded = tileArea / oneTileSize;
+            Console.WriteLine(tilesNeeded);
+            double timeToChangeTiles = tilesNeeded * 0.2;
+            Console.WriteLine(timeToChangeTiles);
+        }
+    }
+}
---
+++ SimpleCalculationsExamProblems/VegetableMarket/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegetablesPrice = double.Parse(Console.ReadLine());
+            double fruitsPrice = double.Parse(Console.ReadLine());
+            int vegetablesAmmount = int.Parse(Console.ReadLine());
+            int fruitsAmmount = int.Parse(Console.ReadLine());
+            double income = (vegetablesAmmount * vegetablesPrice + fruitsAmmount * fruitsPrice) / 1.94;
+            Console.WriteLine(income);
+        }
+    }
+}
