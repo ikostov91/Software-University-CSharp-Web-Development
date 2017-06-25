7f4f6f3a6dc888af0ef29512d684f2ad58ce7b6a
ikostov91 <i.kostov91@gmail.com>
2017-06-25 18:47:42 +0300

    new problems

---
+++ SimpleCalculationsExamProblems/DailyEarnings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyEarnings
+{
+    class Program
+    {
+        static void Main()
+        {
+            int avgWorkDays = int.Parse(Console.ReadLine());
+            double earningPerDay = double.Parse(Console.ReadLine());
+            double courseUSD = double.Parse(Console.ReadLine());
+            double yearlyMoney = avgWorkDays * 12 * earningPerDay;
+            double totalMoney = yearlyMoney + (avgWorkDays * earningPerDay * 2.5);
+            double taxDeducted = totalMoney - (totalMoney * 25 / 100);
+            double finalEarningsPerDay = taxDeducted / 365;
+            Console.WriteLine(Math.Round((finalEarningsPerDay * courseUSD),2));
+        }
+    }
+}
