7e446bd8863549a6dfadc9f9767ec8b18a7fda73
ikostov91 <i.kostov91@gmail.com>
2017-07-02 23:20:49 +0300

    new code

--- ComplexConditions/FruitShop/Program.cs
+++ ComplexConditions/FruitShop/Program.cs
@@ -10,7 +10,93 @@ namespace FruitShop
     {
         static void Main(string[] args)
         {
+            string fruit = Console.ReadLine().ToLower();
+            string day = Console.ReadLine().ToLower();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
 
+            if (day == "monday" || day == "tuesday" || day == "wednesday" || day == "thursday" || day == "friday")
+            {
+                if (fruit == "banana")
+                {
+                    price = 2.50;
+                }
+                else if (fruit == "apple")
+                {
+                    price = 1.20;
+                }
+                else if (fruit == "orange")
+                {
+                    price = 0.85;
+                }
+                else if (fruit == "grapefruit")
+                {
+                    price = 1.45;
+                }
+                else if (fruit == "kiwi")
+                {
+                    price = 2.70;
+                }
+                else if (fruit == "pineapple")
+                {
+                    price = 5.50;
+                }
+                else if (fruit == "grapes")
+                {
+                    price = 3.85;
+                }
+                else
+                {
+                    price = 0;
+                    Console.WriteLine("error");
+                }
+            }
+            else if (day == "saturday" || day == "sunday")
+            {
+                if (fruit == "banana")
+                {
+                    price = 2.70;
+                }
+                else if (fruit == "apple")
+                {
+                    price = 1.25;
+                }
+                else if (fruit == "orange")
+                {
+                    price = 0.90;
+                }
+                else if (fruit == "grapefruit")
+                {
+                    price = 1.60;
+                }
+                else if (fruit == "kiwi")
+                {
+                    price = 3.00;
+                }
+                else if (fruit == "pineapple")
+                {
+                    price = 5.60;
+                }
+                else if (fruit == "grapes")
+                {
+                    price = 4.20;
+                }
+                else
+                {
+                    price = 0;
+                    Console.WriteLine("error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            if (price != 0)
+            {
+                double totalPrice = price * quantity;
+                Console.WriteLine(Math.Round(totalPrice, 2));
+            }
         }
     }
 }
