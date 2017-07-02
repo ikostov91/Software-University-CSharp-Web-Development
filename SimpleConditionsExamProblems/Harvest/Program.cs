b6582d1bea828bc091df8ce6ac52442b03474899
ikostov91 <i.kostov91@gmail.com>
2017-07-02 17:51:12 +0300

    new code

---
+++ SimpleConditionsExamProblems/Harvest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vineyard = int.Parse(Console.ReadLine());
+            double grapesPerSquareMeter = double.Parse(Console.ReadLine());
+            int litersWineNeeded = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+
+            double usableAreaVineyard = vineyard * 40 / 100;
+            double totalGrapesAmmount = usableAreaVineyard * grapesPerSquareMeter;
+            double litersFromAvailableGrapes = totalGrapesAmmount / 2.5;
+
+            if (litersFromAvailableGrapes >= litersWineNeeded)
+            {
+                Console.WriteLine("Good harvest this year! Total wine: {0} liters.", Math.Floor(litersFromAvailableGrapes));
+                double litersLeft = Math.Ceiling(litersFromAvailableGrapes - litersWineNeeded);
+                double litersPerWorker = Math.Ceiling(litersLeft / workers);
+                Console.WriteLine("{0} liters left -> {1} liters per person.", Math.Ceiling(litersLeft), Math.Ceiling(litersPerWorker));
+            }
+            else
+            {
+                double litersNeeded = litersWineNeeded - litersFromAvailableGrapes;
+                Console.WriteLine("It will be a tough winter! More {0} liters wine needed.", Math.Floor(litersNeeded));
+            }
+        }
+    }
+}
---
+++ SimpleConditionsExamProblems/SleepyTomCat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepyTomCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int restDays = int.Parse(Console.ReadLine());
+            int playTimeRestDays = restDays * 127;
+            int playTimeWorkDays = (365 - restDays) * 63;
+            int totalPlaytime = playTimeRestDays + playTimeWorkDays;
+            int difference = Math.Abs(totalPlaytime - 30000);
+            int hours = difference / 60;
+            int minutes = difference % 60;
+            if (totalPlaytime <= 30000)
+            {
+                
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine("{0} hours and {1} minutes less for play", hours, minutes);
+            }
+            else
+            {
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine("{0} hours and {1} minutes more for play", hours, minutes);
+            }
+        }
+    }
+}
