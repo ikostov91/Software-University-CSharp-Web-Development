a92965d357744994c5b70ffc437bc3187aefd57e
ikostov91 <i.kostov91@gmail.com>
2017-07-03 22:40:10 +0300

    new code

---
+++ ComplexConditions/Cinema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string viewingType = Console.ReadLine().ToLower();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            double income = 0;
+
+            switch (viewingType)
+            {
+                case "premiere":
+                    income = rows * columns * 12.00;
+                    break;
+                case "normal":
+                    income = rows * columns * 7.50;
+                    break;
+                case "discount":
+                    income = rows * columns * 5.00;
+                    break;
+            }
+
+            Console.WriteLine("{0:f2} leva", income);
+        }
+    }
+}
