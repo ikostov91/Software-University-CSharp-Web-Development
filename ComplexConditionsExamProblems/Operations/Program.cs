fd9b6b318387dc6f444243b408cde76174439d98
ikostov91 <i.kostov91@gmail.com>
2017-07-06 23:31:37 +0300

    new code

---
+++ ComplexConditionsExamProblems/Operations/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number1 = decimal.Parse(Console.ReadLine());
+            decimal number2 = decimal.Parse(Console.ReadLine());
+            char action = char.Parse(Console.ReadLine());
+            decimal result = 0.00M;
+            string evenOdd = string.Empty;
+
+            if (number2 == 0 && (action == '/' || action == '%'))
+            {
+                Console.WriteLine("Cannot divide {0} by zero", number1);
+            }
+            else
+            {
+                switch (action)
+                {
+                    case '+':
+                        result = number1 + number2;
+                        if (result % 2 == 0)
+                        {
+                            evenOdd = "even";
+                        }
+                        else
+                        {
+                            evenOdd = "odd";
+                        }
+                        Console.WriteLine("{0} + {1} = {2} - {3}", number1, number2, result, evenOdd);
+                        break;
+
+                    case '-':
+                        result = number1 - number2;
+                        if (result % 2 == 0)
+                        {
+                            evenOdd = "even";
+                        }
+                        else
+                        {
+                            evenOdd = "odd";
+                        }
+                        Console.WriteLine("{0} - {1} = {2} - {3}", number1, number2, result, evenOdd);
+                        break;
+
+                    case '*':
+                        result = number1 * number2;
+                        if (result % 2 == 0)
+                        {
+                            evenOdd = "even";
+                        }
+                        else
+                        {
+                            evenOdd = "odd";
+                        }
+                        Console.WriteLine("{0} * {1} = {2} - {3}", number1, number2, result, evenOdd);
+                        break;
+
+                    case '/':
+                            result = number1 / number2;
+                            Console.WriteLine("{0} / {1} = {2:f2}", number1, number2, result);
+                        break;
+
+                    case '%':
+                            result = number1 % number2;
+                            Console.WriteLine("{0} % {1} = {2}", number1, number2, result);       
+                        break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ ComplexConditionsExamProblems/Trip/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            string season = Console.ReadLine().ToLower();
+            string destination = string.Empty;
+            string campOrHotel = string.Empty;
+            decimal moneySpent = 0.00M;
+
+            if (budget <= 100.00M)
+            {
+                destination = "Bulgaria";
+                switch (season)
+                {
+                    case "summer":
+                        campOrHotel = "Camp";
+                        moneySpent = budget * 0.3M;
+                        break;
+                    case "winter":
+                        campOrHotel = "Hotel";
+                        moneySpent = budget * 0.7M;
+                        break;
+                }
+            }
+            else if (budget > 100.00M && budget <= 1000.00M)
+            {
+                destination = "Balkans";
+                switch (season)
+                {
+                    case "summer":
+                        campOrHotel = "Camp";
+                        moneySpent = budget * 0.4M;
+                        break;
+                    case "winter":
+                        campOrHotel = "Hotel";
+                        moneySpent = budget * 0.8M;
+                        break;
+                }
+            }
+            else if (budget > 1000.00M)
+            {
+                destination = "Europe";
+                campOrHotel = "Hotel";
+                moneySpent = budget * 0.9M;
+            }
+
+            Console.WriteLine("Somewhere in " + destination);
+            Console.WriteLine("{0} - {1:f2}", campOrHotel, moneySpent);
+        }
+    }
+}
