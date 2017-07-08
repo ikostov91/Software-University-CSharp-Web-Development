a3ba34ce4bf4b8297b624174de40c84bb59eae95
ikostov91 <i.kostov91@gmail.com>
2017-07-08 19:52:30 +0300

    new code

---
+++ SimpleCalculations/CurrencyConverterWithDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverterWithDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal ammount = decimal.Parse(Console.ReadLine());
+            string firstCurrency = Console.ReadLine().ToLower();
+            string secondCurrency = Console.ReadLine().ToLower();
+
+            var currencies = new Dictionary<string, decimal>()
+            {
+                {"bgn", 1m},
+                {"usd", 1.79549m},
+                {"eur", 1.95583m},
+                {"gbp", 2.53405m}
+            };
+
+            decimal result = (currencies[firstCurrency] / currencies[secondCurrency]) * ammount;
+
+            Console.WriteLine("{0} {1}", Math.Round(result, 2), secondCurrency);
+        }
+    }
+}
