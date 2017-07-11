01a7b9c8c1812520fedbbfcd4027421f77f35132
ikostov91 <i.kostov91@gmail.com>
2017-07-11 23:16:22 +0300

    new code

---
+++ Loops/EqualPairs/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double nextPairSum = 0, previousPairSum = 0;
+
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            double maxDifference = 0;
+            previousPairSum = firstNumber + secondNumber;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                double number2 = double.Parse(Console.ReadLine());
+                nextPairSum = number + number2;
+
+                if (nextPairSum == previousPairSum)
+                {
+                    continue;
+                }
+                else
+                {
+                    double difference = Math.Abs(nextPairSum - previousPairSum);
+                    if (difference > maxDifference)
+                    {
+                        maxDifference = difference;
+                    }
+                }
+
+                previousPairSum = nextPairSum;
+            }
+
+            if (maxDifference == 0)
+            {
+                Console.WriteLine("Yes,value = {0}", previousPairSum);
+            }
+            else
+            {
+                Console.WriteLine("No,maxdiff = {0}", maxDifference);
+            }
+        }
+    }
+}
---
+++ Loops/HalfSumElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0, max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > max)
+                {
+                    max = number;
+                }
+                sum += number;
+            }
+
+            if (max == (sum - max))
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}", max);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = {0}", Math.Abs(max - (sum - max)));
+            }
+        }
+    }
+}
