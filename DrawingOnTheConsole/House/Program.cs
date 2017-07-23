7bc9616f27f7ba2d4ac9a0343f3462dcd51841c2
ikostov91 <i.kostov91@gmail.com>
2017-07-23 17:34:37 +0300

    new code

--- DrawingOnTheConsole/House/Program.cs
+++ DrawingOnTheConsole/House/Program.cs
@@ -10,6 +10,41 @@ namespace House
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int stars = 0, spaces = 0;
+            double upperPart = 0, lowerPart = 0;
+            bool even = true;
+            if (n % 2 != 0)
+            {
+                even = false;
+            }
+
+            if (even) 
+            {
+                spaces = (n - 2) / 2;  
+                stars = 2;            
+            }
+            else
+            {
+                spaces = (n - 1) / 2;
+                stars = 1;
+            }
+
+            upperPart = Math.Ceiling((double)n / 2);
+
+            for (int i = 0; i < upperPart; i++)
+            {
+                Console.WriteLine(new string('-', spaces) + new string('*', stars) + new string('-', spaces));
+                spaces--;
+                stars += 2;
+            }
+
+            lowerPart = Math.Floor((double)n / 2);
+
+            for (int i = 0; i < lowerPart; i++)
+            {
+                Console.WriteLine(new string('|', 1) + new string('*', (n - 2)) + new string('|', 1));
+            }
         }
     }
 }
