6f46c79f0a385d6a668aca10d182b658691d6178
ikostov91 <i.kostov91@gmail.com>
2017-08-14 23:39:38 +0300

    change

--- DrawingOnTheConsoleExamProblems/Butterfly/Program.cs
+++ DrawingOnTheConsoleExamProblems/Butterfly/Program.cs
@@ -22,7 +22,6 @@ namespace Butterfly
                 {
                     Console.WriteLine("{0}\\ /{1}", new string('*', n - 2), new string('*', n - 2));
                 }
-                
             }
 
             Console.WriteLine("{0}{1}{2}", new string(' ', n - 1), '@', new string(' ', n - 1));
