7ea95808bb276f6ecb98170428d1da386328ee99
ikostov91 <i.kostov91@gmail.com>
2017-08-20 22:20:31 +0300

    new code

---
+++ AdvancedLoops/NumbersTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int num = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    num = i + j + 1;
+
+                    if (num > n)
+                    {
+                        num = 2 * n - num;
+                    }
+
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
