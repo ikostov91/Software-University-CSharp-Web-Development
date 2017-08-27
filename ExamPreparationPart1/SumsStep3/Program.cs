0fef34429c1b42de614f7b7553c6663d7659deef
ikostov91 <i.kostov91@gmail.com>
2017-08-27 19:47:04 +0300

    new code

---
+++ ExamPreparationPart1/SumsStep3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumsStep3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum1 = 0, sum2 = 0, sum3 = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
