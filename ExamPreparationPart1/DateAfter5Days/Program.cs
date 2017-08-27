fdb0d1dd77a6ad5e6bb7643df0a8d4026bcbe70a
ikostov91 <i.kostov91@gmail.com>
2017-08-27 19:32:42 +0300

    new code

---
+++ ExamPreparationPart1/DateAfter5Days/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAfter5Days
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+
+            int currentMonthDays = 0;
+
+            switch (month)
+            {
+                case 4:
+                case 6:
+                case 9:
+                case 11: currentMonthDays = 30; break;
+                case 2:  currentMonthDays = 28; break;
+                default: currentMonthDays = 31; break;
+            }
+
+            int newDays = days + 5;
+
+            if (newDays > currentMonthDays)
+            {
+                newDays -= currentMonthDays;
+                if (month == 12)
+                {
+                    month = 1;
+                }
+                else
+                {
+                    month++;
+                }
+            }
+
+            Console.WriteLine("{0}.{1:D2}", newDays, month);
+            if (month < 10)
+        }
+    }
+}
---
+++ ExamPreparationPart1/PointOnSegment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOnSegment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int point = int.Parse(Console.ReadLine());
+
+            int x2 = Math.Max(first,second);
+            int x1 = Math.Min(first, second);
+
+            if (point >= x1 && point <= x2)
+            {
+                Console.WriteLine("in");
+
+                Console.WriteLine("{0}", Math.Min(x2 - point,point - x1));
+            }
+            else
+            {
+                Console.WriteLine("out");
+
+                if (point > x2)
+                {
+                    Console.WriteLine("{0}", point - x2);
+                }
+                else
+                {
+                    Console.WriteLine("{0}", x1 - point);
+                }
+            }
+        }
+    }
+}
