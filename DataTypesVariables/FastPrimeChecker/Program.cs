0b851ce18c1e1a9af3743ec460ea285c8b1d68b2
ikostov91 <i.kostov91@gmail.com>
2017-10-01 22:05:09 +0300

    Added two new problems for homework

---
+++ DataTypesVariables/FastPrimeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = 2; currentNumber <= number; currentNumber++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j <= Math.Sqrt(currentNumber); j++)
+                {
+                    if (currentNumber % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{currentNumber} -> {isPrime}");
+            }
+        }
+    }
+}
