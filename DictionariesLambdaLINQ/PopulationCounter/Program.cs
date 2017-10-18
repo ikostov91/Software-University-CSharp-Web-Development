84b8aac2fc107bc18126f786c6077d35dfe11eef
Ivaylo <i.kostov91@gmail.com>
2017-10-18 09:18:28 +0300

    New homework in Dictionaries

---
+++ DictionariesLambdaLINQ/PopulationCounter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopulationCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> populationData = new Dictionary<string, Dictionary<string, int>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "report")
+            {
+                string[] data = input.Split('|').ToArray();
+                string city = data[0];
+                Console.WriteLine(city);
+                string country = data[1];
+                Console.WriteLine(country);
+                int population = int.Parse(data[2]);
+                Console.WriteLine(population);
+
+                if (populationData.ContainsKey(country))
+                {
+                    if (populationData[country].ContainsKey(city))
+                    {
+                        populationData[country][city] += population;
+                    }
+                    else
+                    {
+                        populationData[country].Add(city, population);
+                    }
+                }
+                else
+                {
+                    populationData.Add(country, new Dictionary<string, int>());
+                    populationData[country].Add(city, population);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var country in populationData.OrderByDescending(x => x.Value.Sum(y => y.Value)))
+            {
+                int totalPopulation = 0;
+                totalPopulation = country.Value.Sum(total => total.Value);
+
+                Console.WriteLine($"{country.Key} (total population: {totalPopulation})");
+
+                foreach (var city in country.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"=>{city.Key}: {city.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ DictionariesLambdaLINQ/userLogs/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, SortedDictionary<string, int>> users = new SortedDictionary<string, SortedDictionary<string, int>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] log = input.Split(new char[] { '=', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string user = log[5];
+                string ipAddress = log[1];
+                int counter = 1;
+
+                if (users.ContainsKey(user))
+                {
+                    if (users[user].ContainsKey(ipAddress))
+                    {
+                        users[user][ipAddress] += 1;
+                    }
+                    else
+                    {
+                        users[user].Add(ipAddress, counter);
+                    }
+                }
+                else
+                {
+                    users.Add(user, new SortedDictionary<string, int>());
+                    users[user].Add(ipAddress, 1);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Key}:");
+
+                List<string> items = new List<string>();
+
+                foreach (var ip in user.Value)
+                {
+                    var text = $"{ip.Key} => {ip.Value}";
+                    items.Add(text);
+                }
+
+                Console.WriteLine($"{String.Join(", ",items)}.");
+            }
+        }
+    }
+}
