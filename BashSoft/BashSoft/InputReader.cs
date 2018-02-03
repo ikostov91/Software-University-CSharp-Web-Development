a62d567a611b6237f16afa392914b4e164a939f7
Ivaylo <i.kostov91@gmail.com>
2018-02-03 23:06:13 +0200

    BashSoft first commit

---
+++ BashSoft/BashSoft/InputReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BashSoft
+{
+    public static class InputReader
+    {
+        private const string endCommand = "quit";
+        public static void StartReadingCommands()
+        {
+            string input = Console.ReadLine();
+
+            while (input != endCommand)
+            {
+                CommandInterpreter.InterpretCommand(input);
+                OutputWriter.WriteMessage($"{SessionData.currentPath}>");
+                input = Console.ReadLine();
+                input = input.Trim();
+            }
+        }
+    }
+}
