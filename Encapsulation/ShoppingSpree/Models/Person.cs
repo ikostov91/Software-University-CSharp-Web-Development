108371f7d107f7581436ce5be8d884b5e282494e
Ivaylo <i.kostov91@gmail.com>
2018-03-05 15:02:37 +0200

    new code

---
+++ Encapsulation/ShoppingSpree/Models/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Person
+{
+    private string name;
+    private decimal money;
+    private List<Product> products;
+
+    public string Name
+    {
+        get { return this.name; }
+        private set
+        {
+            if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+            this.name = value;
+        }
+    }
+
+    public decimal Money
+    {
+        get { return this.money; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Money cannot be negative");
+            }
+            this.money = value;
+        }
+    }
+
+    public List<Product> Products
+    {
+        get { return this.products; }
+    }
+
+    public Person(string name, decimal money)
+    {
+        this.Name = name;
+        this.Money = money;
+        this.products = new List<Product>();
+    }
+
+    public bool AttemptToBuyProduct(Product product)
+    {
+        if (product.Cost > this.Money)
+        {
+            
+            return false;
+        }
+
+        this.products.Add(product);
+        this.Money -= product.Cost;
+        return true;
+        
+    }
+}
+
---
+++ Encapsulation/ShoppingSpree/Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+public class Product
+{
+    private string name;
+    private decimal cost;
+
+    public string Name
+    {
+        get { return this.name; }
+        set
+        {
+            if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+            this.name = value;
+        }
+    }
+
+    public decimal Cost
+    {
+        get { return this.cost; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Money cannot be negative");
+            }
+            this.cost = value;
+        }
+    }
+
+    public Product(string name, decimal cost)
+    {
+        this.Name = name;
+        this.Cost = cost;
+    }
+}
+
